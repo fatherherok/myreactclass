f6f973affcaeb20c48fb1d976301286a099fac05
Olukayode Fadairo <101965202+fatherherok@users.noreply.github.com>
2022-07-19 23:09:06 +0100

    Initial commit

---
+++ src/components/List.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function List() {
+  //the React components e.g Heading should be in capital letter
+  return (
+    <ul>
+      <li>Bacon</li>
+      <li>Jamon</li>
+      <li>Noodles</li>
+    </ul>
+  );
+}
+
+export default List;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./components/App";
+//you may choose to use the extenstion App.jsx
+
+ReactDOM.render(<App />, document.getElementById("root"));
